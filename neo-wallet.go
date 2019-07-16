11f570fdb4126f00df0e0085df08173520d8f61e
Guilherme Campos <guilherme.campos@consenys.net>
2019-07-16 17:34:06 +0100

    minor change

--- neo-wallet.go
+++ neo-wallet.go
@@ -22,49 +22,10 @@ type (
 )
 
 // NewPrivateKeyFromWIF creates a PrivateKey struct using a WIF.
-func NewPrivateKeyFromWIF(wif []byte) (*PrivateKey, error) {
-	// base58 := utility.NewBase58()
-
-	// decodedWIF, _ := base58.Decode(wif)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	// if len(decodedWIF) != 38 {
-	// 	return nil, fmt.Errorf(
-	// 		"Expected length of decoded WIF to be 38, got: %d", len(decodedWIF),
-	// 	)
-	// }
-
-	// if decodedWIF[0] != 0x80 {
-	// 	return nil, fmt.Errorf(
-	// 		"Expected first byte of decoded WIF to be '0x80', got: %x", decodedWIF[0],
-	// 	)
-	// }
-
-	// if decodedWIF[33] != 0x01 {
-	// 	return nil, fmt.Errorf(
-	// 		"Expected 34th byte of decoded WIF to be '0x01', got: %x", decodedWIF[33],
-	// 	)
-	// }
-
-	// subString := decodedWIF[:len(decodedWIF)-4]
-
-	// rawFirstSHA := sha256.Sum256([]byte(subString))
-	// firstSHA := rawFirstSHA[:]
-
-	// rawSecondSHA := sha256.Sum256(firstSHA)
-	// secondSHA := rawSecondSHA[:]
-
-	// firstFourBytes := secondSHA[:4]
-	// lastFourBytes := decodedWIF[len(decodedWIF)-4 : len(decodedWIF)]
-
-	// if !bytes.Equal(firstFourBytes, lastFourBytes) {
-	// 	return nil, fmt.Errorf("WIF failed checksum validation")
-	// }
+func NewPrivateKeyFromWIF(seed []byte) (*PrivateKey, error) {
 	fmt.Println("Neo Address generation ")
 	return &PrivateKey{
-		bytes: wif[1:33],
+		bytes: seed[1:33],
 	}, nil
 }
 
