725f7c8543e5eb0eadccda25a9d71704145eb6eb
Guilherme Campos <guilherme.campos@consenys.net>
2019-07-16 12:02:01 +0100

    step by step

--- go-key-gen.go
+++ go-key-gen.go
@@ -3,42 +3,20 @@ package main
 import (
 	"encoding/hex"
 	"fmt"
-	"strings"
 
-	"golang.org/x/crypto/sha3"
-
-	"github.com/tyler-smith/go-bip32"
 	"github.com/tyler-smith/go-bip39"
 )
 
-func PublicKeyBytesToEthereumAddress(publicKey []byte) string {
-	var buf []byte
+func GenerateBitcoinHDWAllet() {
 
-	hash := sha3.NewLegacyKeccak256()
-	hash.Write(publicKey[1:]) // remove EC prefix 04
-	buf = hash.Sum(nil)
-	address := buf[12:]
-
-	return "0x" + strings.ToUpper(hex.EncodeToString(address))
 }
 
 func main() {
-	// Generate a mnemonic for memorization or user-friendly seeds
 	entropy, _ := bip39.NewEntropy(256)
 	mnemonic, _ := bip39.NewMnemonic(entropy)
+	seed := bip39.NewSeed(mnemonic, "pass")
 
-	// Generate a Bip32 HD wallet for the mnemonic and a user supplied password
-	seed := bip39.NewSeed(mnemonic, "Secret Passphrase")
-
-	masterKey, _ := bip32.NewMasterKey(seed)
-	publicKey := masterKey.PublicKey()
-
-	// Display mnemonic and keys
-	fmt.Println("Mnemonic: ", mnemonic)
+	fmt.Println("Entropy: ", hex.EncodeToString(entropy))
+	fmt.Println("Menmonic: ", mnemonic)
 	fmt.Println("Seed: ", hex.EncodeToString(seed))
-	fmt.Println("BIP32 Root Key: ", masterKey)
-	fmt.Println("Master public key: ", publicKey)
-
-	ethPublicAddress := PublicKeyBytesToEthereumAddress(publicKey.Key)
-	fmt.Println(ethPublicAddress)
 }
