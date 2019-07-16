f3a4e2ddaca6bb83a6462d50ae1940e3b2380495
Guilherme Campos <guilherme.campos@consenys.net>
2019-07-16 14:12:14 +0100

    Ethereum and Bitcoin working

---
+++ eth-wallet.go
@@ -0,0 +1,187 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"errors"
+	"sync"
+
+	"github.com/btcsuite/btcd/btcec"
+
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcutil/hdkeychain"
+	"github.com/ethereum/go-ethereum/accounts"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/tyler-smith/go-bip39"
+)
+
+// Wallet is the underlying wallet struct.
+type Wallet struct {
+	mnemonic      string
+	masterKey     *hdkeychain.ExtendedKey
+	masterPrivKey *btcec.PrivateKey
+	masterPubKey  *btcec.PublicKey
+	seed          []byte
+	url           accounts.URL
+	paths         map[common.Address]accounts.DerivationPath
+	accounts      []accounts.Account
+	stateLock     sync.RWMutex
+}
+
+func NewWallet(seed []byte) (*Wallet, error) {
+	masterKey, err := hdkeychain.NewMaster(seed, &chaincfg.MainNetParams)
+	if err != nil {
+		return nil, err
+	}
+
+	privKey, _ := masterKey.ECPrivKey()
+	pubKey, _ := masterKey.ECPubKey()
+	return &Wallet{
+		masterKey:     masterKey,
+		masterPrivKey: privKey,
+		masterPubKey:  pubKey,
+		seed:          seed,
+		accounts:      []accounts.Account{},
+		paths:         map[common.Address]accounts.DerivationPath{},
+	}, nil
+}
+
+// NewFromMnemonic returns a new wallet from a BIP-39 mnemonic.
+func NewFromMnemonic(mnemonic string, passphrase string) (*Wallet, error) {
+	if mnemonic == "" {
+		return nil, errors.New("mnemonic is required")
+	}
+
+	if !bip39.IsMnemonicValid(mnemonic) {
+		return nil, errors.New("mnemonic is invalid")
+	}
+
+	seed, err := NewSeedFromMnemonic(mnemonic, passphrase)
+	if err != nil {
+		return nil, err
+	}
+
+	wallet, err := NewWallet(seed)
+	if err != nil {
+		return nil, err
+	}
+	wallet.mnemonic = mnemonic
+
+	return wallet, nil
+}
+
+// NewSeedFromMnemonic returns a BIP-39 seed based on a BIP-39 mnemonic.
+func NewSeedFromMnemonic(mnemonic string, passphrase string) ([]byte, error) {
+	if mnemonic == "" {
+		return nil, errors.New("mnemonic is required")
+	}
+
+	return bip39.NewSeedWithErrorChecking(mnemonic, passphrase)
+}
+
+// NewMnemonic returns a randomly generated BIP-39 mnemonic using 128-256 bits of entropy.
+func NewMnemonic(bits int) (string, error) {
+	entropy, err := bip39.NewEntropy(bits)
+	if err != nil {
+		return "", err
+	}
+	return bip39.NewMnemonic(entropy)
+}
+
+// Derive implements accounts.Wallet, deriving a new account at the specific
+// derivation path. If pin is set to true, the account will be added to the list
+// of tracked accounts.
+func (w *Wallet) Derive(path accounts.DerivationPath, pin bool) (accounts.Account, error) {
+	// Try to derive the actual account and update its URL if successful
+	w.stateLock.RLock() // Avoid device disappearing during derivation
+
+	address, err := w.deriveAddress(path)
+
+	w.stateLock.RUnlock()
+
+	// If an error occurred or no pinning was requested, return
+	if err != nil {
+		return accounts.Account{}, err
+	}
+
+	account := accounts.Account{
+		Address: address,
+		URL: accounts.URL{
+			Scheme: "",
+			Path:   path.String(),
+		},
+	}
+
+	if !pin {
+		return account, nil
+	}
+
+	// Pinning needs to modify the state
+	w.stateLock.Lock()
+	defer w.stateLock.Unlock()
+
+	if _, ok := w.paths[address]; !ok {
+		w.accounts = append(w.accounts, account)
+		w.paths[address] = path
+	}
+
+	return account, nil
+}
+
+// DeriveAddress derives the account address of the derivation path.
+func (w *Wallet) deriveAddress(path accounts.DerivationPath) (common.Address, error) {
+	publicKeyECDSA, err := w.derivePublicKey(path)
+	if err != nil {
+		return common.Address{}, err
+	}
+
+	address := crypto.PubkeyToAddress(*publicKeyECDSA)
+	return address, nil
+}
+
+// DerivePublicKey derives the public key of the derivation path.
+func (w *Wallet) derivePublicKey(path accounts.DerivationPath) (*ecdsa.PublicKey, error) {
+	privateKeyECDSA, err := w.derivePrivateKey(path)
+	if err != nil {
+		return nil, err
+	}
+
+	publicKey := privateKeyECDSA.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		return nil, errors.New("failed to get public key")
+	}
+
+	return publicKeyECDSA, nil
+}
+
+// DerivePrivateKey derives the private key of the derivation path.
+func (w *Wallet) derivePrivateKey(path accounts.DerivationPath) (*ecdsa.PrivateKey, error) {
+	var err error
+	key := w.masterKey
+	for _, n := range path {
+		key, err = key.Child(n)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	privateKey, err := key.ECPrivKey()
+	privateKeyECDSA := privateKey.ToECDSA()
+	if err != nil {
+		return nil, err
+	}
+
+	return privateKeyECDSA, nil
+}
+
+// MustParseDerivationPath parses the derivation path in string format into
+// []uint32 but will panic if it can't parse it.
+func MustParseDerivationPath(path string) accounts.DerivationPath {
+	parsed, err := accounts.ParseDerivationPath(path)
+	if err != nil {
+		panic(err)
+	}
+
+	return parsed
+}
--- go-key-gen.go
+++ go-key-gen.go
@@ -3,19 +3,48 @@ package main
 import (
 	"encoding/hex"
 	"fmt"
+	"log"
+
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcutil/hdkeychain"
+	hdwallet "github.com/miguelmota/go-ethereum-hdwallet"
 )
 
-func GenerateBitcoinAddress() {
+func GenerateEthereumWallet(menmoninc string, seed string) {
+	wallet, _ := NewFromMnemonic(menmoninc, seed)
+	path := hdwallet.MustParseDerivationPath("m/44'/60'/0'/0/0")
+	account, err := wallet.Derive(path, false)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	fmt.Println("Master root key: ", wallet.masterKey)
+	fmt.Println("Private Extended Key?:", wallet.masterKey.IsPrivate())
+	fmt.Println("Master private key: ", wallet.masterPrivKey)
+	fmt.Println("Master public key: ", wallet.masterPubKey)
+	fmt.Println("Ethereum Address: ", account.Address.Hex())
+}
+
+func GenerateBitcoinWallet(seed []byte) {
+	// Generate a new master node using the seed.
+	key, err := hdkeychain.NewMaster(seed, &chaincfg.MainNetParams)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	address, _ := key.Address(&chaincfg.MainNetParams)
+	// Show that the generated master node extended key is private.
+	fmt.Println("Bitcoin Address: ", address)
 }
 
 func main() {
 	mnemonic, _ := NewMnemonic(256)
-	wallet, _ := NewFromMnemonic(mnemonic, "pass")
+	seed, _ := NewSeedFromMnemonic(mnemonic, "pass")
+
+	GenerateEthereumWallet(mnemonic, hex.EncodeToString(seed))
+	GenerateBitcoinWallet(seed)
 
 	fmt.Println("Menmonic: ", mnemonic)
-	fmt.Println("Seed: ", hex.EncodeToString(wallet.seed))
-	fmt.Println("Master root key: ", wallet.masterKey)
-	fmt.Println("Master private key: ", wallet.masterPrivKey)
-	fmt.Println("Master public key: ", wallet.masterPubKey)
+	fmt.Println("Seed: ", hex.EncodeToString(seed))
 }
