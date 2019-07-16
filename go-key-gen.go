373449427ebbeec7c7df2ae3ef6b0da95fe15dd2
Guilherme Campos <guilherme.campos@consenys.net>
2019-07-16 16:33:56 +0100

    Monero added

--- go-key-gen.go
+++ go-key-gen.go
@@ -6,6 +6,8 @@ import (
 	"log"
 
 	ethhdwallet "github.com/miguelmota/go-ethereum-hdwallet"
+	"github.com/patcito/monero"
+	"github.com/patcito/monero/crypto"
 	"github.com/stellar/go/keypair"
 	bithdwallet "github.com/wemeetagain/go-hdwallet"
 )
@@ -45,6 +47,19 @@ func GenerateTronWallet(seed []byte) {
 	addressFromKey(hex.EncodeToString(key.D.Bytes()))
 }
 
+func GenerateMoneroWallet(seed []byte) {
+	var seed32 [32]byte
+	copy(seed32[:], seed)
+	var secret [32]byte
+	var view_secret [32]byte
+	var spend_secret [32]byte
+	crypto.SecretFromSeed(&secret, &seed32)
+	account, _ := monero.RecoverAccount(secret)
+	spend_secret = account.Secret()
+	crypto.ViewFromSpend(&view_secret, &spend_secret)
+	fmt.Printf("Monero Address: %s\n", account.Address().String())
+}
+
 func main() {
 	passphrase := "pass"
 	mnemonic, _ := NewMnemonic(256)
@@ -58,5 +73,6 @@ func main() {
 	GenerateBitcoinWallet(seed)
 	GenerateStellarWallet(seed)
 	GenerateTronWallet(seed)
+	GenerateMoneroWallet(seed)
 
 }
